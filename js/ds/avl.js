ae4c8bff4711c2c531d478e08b8ef266b7842c99
Carol1992 <1689597038@qq.com>
2021-12-15 18:25:37 +0800

    avl

--- js/ds/avl.js
+++ js/ds/avl.js
@@ -6,20 +6,37 @@
  * retracing: after insertion or deletion, check each of the node's ancestors for consistency with the variants of avl tree.
  * tree rotations: move the keys only "vertically", so that the in-order sequence of the keys is fully preserved
  * there are 4 possible variants of the vialation: (node Z, balance factor BF, parent X)
- * right right: Z is the right child of X and BF(Z) >= 0
- * left left: Z is the left child of X and BF(Z) < 0
- * left right: Z is the left child of X and BF(Z) > 0
- * right left: Z is the right child of X and BF(Z) < 0
+    * right right: Z is the right child of X and BF(Z) >= 0
+    * left left: Z is the left child of X and BF(Z) < 0
+    * left right: Z is the left child of X and BF(Z) > 0
+    * right left: Z is the right child of X and BF(Z) < 0
  * with different vialation, we can perform different rotations:
- * right right: simple rotation rotate_Left
- * left left: simple rotation rotate_Right
- * left right: double rotation rotate_RightLeft
- * right left: double rotation rotate_LeftRight
+    * right right: simple rotation rotate_Left
+    * left left: simple rotation rotate_Right
+    * left right: double rotation rotate_LeftRight
+    * right left: double rotation rotate_RightLeft
+ * pesudo code
+ *  if (tree is left heavy) {
+      if(tree's subtree is left heavy){
+        single right rotation;
+      } else {
+        single left rotation;
+        single right rotation;
+      }
+    } else if (tree is right heavy) {
+      if(tree's subtree is left heavy){
+        single right rotation;
+        single left rotation;
+      } else {
+        single left rotation;
+      }
+    }
  * */
 let root = null;
 class Node {
-  constructor(key) {
+  constructor(key, parent) {
     this.key = key;
+    this.parent = parent;
     this.left = this.right = null;
     this.leftH = this.rightH = 0;
   }
@@ -27,21 +44,20 @@ class Node {
     return this.rightH - this.leftH;
   }
 }
-function insert(node, key) {
-  if(!node) {
-    let node = new Node(key);
+function insert(node, key, parent) {
+  if (!node) {
+    let node = new Node(key, parent);
     return node;
-  } 
-  else {
-    if(key < node.key) {
-      node.left = insert(node.left, key);
+  } else {
+    if (key < node.key) {
+      node.left = insert(node.left, key, node);
       node.leftH++;
-    } else if(key > node.key) {
-      node.right = insert(node.right, key);
+    } else if (key > node.key) {
+      node.right = insert(node.right, key, node);
       node.rightH++;
     }
-    if(node.bf() > 1 || node.bf() < -1) {
-      rebalancing(node);
+    if (node.bf() > 1 || node.bf() < -1) {
+      node = rebalancing(node);
     }
     return node;
   }
@@ -66,24 +82,68 @@ function deletes(node, key) {
     node.right = delete(node.right, key);
   }
   if(node.bf() > 1 || node.bf() < -1) {
-    rebalancing(node);
+    node = rebalancing(node);
   }
   return node;
 }
-function getMin(node) {
+function getMinNode(node) {
   let min = node.key;
-  while(node.left !== null) {
+  while (node.left !== null) {
     min = node.left.key;
     node = node.left;
   }
   return min;
 }
 function rebalancing(node) {
-  // console.log(node);
+  let w = node;
+  if (node.bf() < -1) {
+    let subtree = node.left;
+    if (subtree.bf() < 0) {
+      w = rotateRight(node);
+    } else {
+      /**
+       * We first perform the left rotation on the left subtree of node.
+       */
+      w.left = rotateLeft(subtree);
+      // then, rotate right
+      w = rotateRight(w);
+    }
+  } else if (node.bf() > 1) {
+    let subtree = node.right;
+    if (subtree.bf() < 0) {
+      w.right = rotateRight(subtree);
+      w = rotateLeft(w);
+    } else {
+      w = rotateLeft(node);
+    }
+  }
+  return w;
+}
+function rotateLeft(node) {
+  let w = node.right;
+  w.parent = node.parent;
+  node.parent = w;
+  node.right = w.left;
+  if (w.left !== null) w.left.parent = node;
+  w.left = node;
+  node.leftH++;
+  node.rightH--;
+  return w;
+}
+function rotateRight(node) {
+  let w = node.left;
+  w.parent = node.parent;
+  node.parent = w;
+  node.left = w.right;
+  if (w.right !== null) w.right.parent = node;
+  w.right = node;
+  node.leftH--;
+  node.rightH++;
+  return w;
 }
 function test() {
   const list = [19, 44, 55, 3, 22, 67, 12, 7, 8, 9, 1234];
-  list.forEach(l => root = insert(root, l));
+  list.forEach((l) => (root = insert(root, l, root)));
 }
 function preorder_print(tree) {
   if (!tree instanceof Node || !tree) return;
@@ -92,5 +152,5 @@ function preorder_print(tree) {
   preorder_print(tree.right);
 }
 test();
-// preorder_print(root);
+preorder_print(root);
 
