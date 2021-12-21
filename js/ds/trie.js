f9f3d576eaf5e1948144dc639a5782dac9421f18
Carol1992 <1689597038@qq.com>
2021-12-21 14:51:22 +0800

    trie

--- js/ds/trie.js
+++ js/ds/trie.js
@@ -1,29 +1,32 @@
 /**
  * a specific type of tree structure, also called prefix tree or digital tree.
- * all the children of a node have a common prefix of the string associated with that parent node, 
+ * all the children of a node have a common prefix of the string associated with that parent node,
  * and the root is associated with the empty string
  * application: dictionary, autocomplete, spell checking, replacement for hashtables, DFSA representation
  */
 class Node {
-  constructor() {
+  constructor(char = "") {
     this.children = [];
     this.value = null;
-    this.key = "";
+    this.char = char;
+    this.isEndOfNode = false;
   }
 }
-function insert(node, key, value) {
-  if (node === null) {
-    return new Node();
-  }
+function insert(root, key, value) {
+  let node = root;
   for (let char of key) {
-    if (node.children.includes(char)) {
-      node = node.children[char];
+    let idx = node.children.findIndex((n) => n.char === char);
+    if (idx !== -1) {
+      node = node.children[idx];
     } else {
-      node.children.push(new Node());
+      let nnode = new Node(char);
+      node.children.push(nnode);
+      node = nnode;
     }
   }
+
+  node.isEndOfNode = true;
   node.value = value;
-  return node;
 }
 function getVal(node, key) {
   if (node === null) return node;
@@ -37,5 +40,24 @@ function getVal(node, key) {
   return node.value;
 }
 
-function print(node) {}
+function print(node, chars = []) {
+  if (node.isEndOfNode) {
+    console.log(chars.join(""), node.value);
+  } else {
+    node.children.forEach((c) => {
+      print(c, chars.concat(c.char));
+    });
+  }
+}
 
+function test() {
+  let root = new Node();
+  let list = ["tree", "map", "try", "mouse", "apple"];
+  let val = [3, 5, 1, 2, 6];
+  list.forEach((l, i) => {
+    insert(root, list[i], val[i]);
+  });
+  print(root);
+}
+
+test();
