9a491d00cb017bd1595dee0394e0a9877bf132c2
Carol1992 <1689597038@qq.com>
2021-12-29 18:19:19 +0800

    ml

---
+++ ml/desicion-tree.js
@@ -0,0 +1,16 @@
+/**
+ * 1. find the best attribute and place it on the root node;
+ * 2. and then, split the dataset into subsets. each subset should have the same value for an attribute.
+ * 3. find leaf nodes in all branches by repeating 1 and 2 on each subsets
+ *
+ * Gini index ?
+ * Information gain ?
+ * Entropy ?
+ * Accuracy score ?
+ * Confusion Matrix ?
+ *
+ * supervised learning vs unsupervised learning (need labelled dataset or not ?)
+ * classification vs regression (discrete or continuous ?)
+ *
+ *
+ */
